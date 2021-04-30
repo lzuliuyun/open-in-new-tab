2f49b8a93eeab6907e047bf64fbb6a9c25ce1c2f
lzuliuyun <lzuliuyun@gmail.com>
2021-04-30 23:12:01 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         新标签页打开
+// @namespace    neo_world_js
+// @version      0.1
+// @description  open in new tab (youtube)
+// @author       neoWorld
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.onload = () => {
+    [...document.querySelectorAll('a')].forEach((ele) => {
+      ele.setAttribute('target', '_blank');
+      const href = ele.getAttribute('href');
+      ele.onclick = (e) => {
+        e.stopPropagation();
+        window.open(href, '_blank');
+        return false;
+      };
+
+      ele.setAttribute('href', '#')
+    });
+  };
+
+})();
