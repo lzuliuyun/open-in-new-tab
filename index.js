5bacc42f7abb62550e75270a4feb459d0a5fd5fb
lzuliuyun <lzuliuyun@gmail.com>
2021-05-14 01:25:29 +0800

    feat: 新增页面，并优化逻辑

--- index.js
+++ index.js
@@ -13,18 +13,37 @@
 (function () {
   "use strict";
 
-  const itemSelectors = ["#dismissible a"];
+  const selectorPathMap = {
+    "/": {
+      observeEle: "#page-manager",
+      aEle: ['#dismissible a[href^="/"]'],
+    },
+    "/results": {
+      observeEle: "#page-manager",
+      aEle: ['#dismissible a[href^="/"]'],
+    },
+    "/channel": {
+      observeEle: "#page-manager",
+      aEle: ['#dismissible a[href^="/"]'],
+    },
+    "/watch": {
+      observeEle: "#secondary",
+      aEle: ['#dismissible a[href^="/"]'],
+    },
+    "/playlist": {
+      observeEle: "#page-manager",
+      aEle: ['#content a[href^="/"]'],
+    },
+    "/user": {
+      observeEle: "#page-manager",
+      aEle: ['#dismissible a[href^="/"]'],
+    },
+  };
 
   const updateAEle = function (selector) {
     const aEle = [...document.querySelectorAll(selector)];
 
     aEle.forEach((ele) => {
-      if (
-        ele.getAttribute("href") === "#" &&
-        ele.getAttribute("target") === "_blank"
-      )
-        return;
-
       ele.setAttribute("target", "_blank");
       const href = ele.getAttribute("href");
       ele.onclick = (e) => {
@@ -37,26 +56,47 @@
     });
   };
 
-  const updateAEleBatch = function () {
-    itemSelectors.forEach((curSelector) => {
+  const updateAEleBatch = function (aEleList) {
+    aEleList.forEach((curSelector) => {
       updateAEle(curSelector);
     });
   };
 
-  const initObserver = function (selectors) {
-    selectors.forEach((curSelector) => {
-      const curEle = document.querySelector(curSelector);
-      if (!curEle) return;
+  const getSelectorAtCurPath = function () {
+    const { pathname } = window.location;
+    const secondIndex = pathname.indexOf("/", 2);
+    const path =
+      secondIndex === -1 ? pathname : pathname.substr(0, secondIndex);
 
-      new MutationObserver(updateAEleBatch).observe(curEle, {
-        childList: true,
-        subtree: true, // 监视子孙节点
-      });
+    return (
+      selectorPathMap[path] || {
+        observeEle: "#page-manager",
+        aEle: ["#dismissible a"],
+      }
+    );
+  };
+
+  const initObserver = function (selector) {
+    const observeEle = document.querySelector(selector.observeEle);
+    if (!observeEle) return;
+
+    new MutationObserver(() => {
+      updateAEleBatch(selector.aEle);
+    }).observe(observeEle, {
+      childList: true,
+      subtree: true, // 监视子孙节点
     });
   };
 
+  const selector = getSelectorAtCurPath();
+
   window.onload = () => {
-    initObserver(["#content"]);
-    updateAEleBatch();
+    initObserver(selector);
+    updateAEleBatch(selector.aEle);
   };
+
+  window.addEventListener("popstate", () => {
+    initObserver(selector);
+    updateAEleBatch(selector.aEle);
+  });
 })();
